ca8c872ba070576871775f5a4d649f3cd00d79dd
Elwing-Chou <birdfan8814@gmail.com>
2025-04-29 11:43:28 +0800

    Add files via upload

---
+++ tibame04291.py
@@ -0,0 +1,6 @@
+import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
